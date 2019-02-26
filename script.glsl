926bef4db35a6ce13b9d5530747a841120fae81d
Chester Abrahams <43809102+ChesterOfTheSemester@users.noreply.github.com>
2019-02-26 09:56:25 +0000

    Create script.glsl

---
+++ script.glsl
@@ -0,0 +1,21 @@
+float AND(float f, float c)
+{
+    float rtn = 0.0,
+          n = 1.0;
+          
+    for(int i=0; i<32; i++)
+    {
+        if (f - 2.0 * (f / 2.0) == 1.0
+        && (c - 2.0 * (c / 2.0) == 1.0))
+            rtn += n;
+            
+        f = f / 2.0;
+        c = c / 2.0;
+        n = n * 2.0;
+        
+        if(!(f > 0.0 && c > 0.0))
+            break;
+    }
+    
+    return rtn;
+}

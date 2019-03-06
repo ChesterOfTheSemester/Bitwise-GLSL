b75c348305fc7fbf6625c0011825057139dd7e06
Chester Abrahams <43809102+ChesterOfTheSemester@users.noreply.github.com>
2019-03-06 11:28:42 +0000

    Update 2D-vertex-rotation.glsl

--- 2D-vertex-rotation.glsl
+++ 2D-vertex-rotation.glsl
@@ -1,9 +1,13 @@
 void main()
 {
+  // Position to center point
+
   //                   LowP PI
   float angle = 80.0 * (3.1415 / 180.0);
   vec2 u_rotation = vec2(sin(angle), cos(angle));     
   
   position_2d = vec2(position_2d.x * u_rotation.y + position_2d.y * u_rotation.x,
                      position_2d.y * u_rotation.y - position_2d.x * u_rotation.x);
+                     
+  // Position back to original point
 }
